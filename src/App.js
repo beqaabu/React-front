d8a67f72f09cc23cd93b1cea2ea87d05f5ed808f
bekaabuladze <beka@reown.io>
2021-05-03 23:46:04 +0400

    React front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Dropdown from './Dropdown'
+
+const items = [ 
+  { id: 'BTC', name: 'Bitcoin'},           
+  { id: 'BCH', name: 'Bitcoin Cash' },
+  { id: 'LTC', name: 'Litecoin' },
+  { id: 'ZEC', name: 'Zcash' },
+  { id: 'USDT', name: 'USDt' },
+  { id: 'XMR', name: 'Monero' },
+  { id: 'DASH', name: 'Dash' },
+  { id: 'ETC', name: 'Ethereum Classic' },
+  { id: 'ETH', name: 'Ethereum' },
+  { id: 'BNB', name: 'Binance Coin' },
+  { id: 'USDC', name: 'USDC' },
+  { id: 'DAI', name: 'Dai' },
+  { id: 'TUSD', name: 'TrueUSD' },
+  { id: 'SAI', name: 'Sai' },
+  { id: 'SPANK', name: 'SpankChain' },
+  { id: 'BSV', name: 'Bitcoin (SV)' },
+  { id: 'GRIN', name: 'Grincoin' },
+  { id: 'L-BTC', name: 'Bitcoin (Liquid)' },
+  { id: 'GRS', name: 'Groestlcoin' },
+  { id: 'TBTC', name: 'Bitcoin Testnet' },
+  { id: 'DOGE', name: 'Dogecoin' },
+  { id: 'VIA', name: 'Viacoin' },
+  { id: 'PPC', name: 'Peercoin' },
+  { id: 'USD', name: 'US Dollar' },
+  { id: 'OMNI', name: 'Omni Token' },
+  { id: 'PAX', name: 'Paxos Standard' },
+  { id: 'BTC2', name: 'Bitcoin (Core Cash)' },
+  { id: 'USDH', name: 'HonestCoin' },
+  { id: 'L-BCH', name: 'Bitcoin Cash (Liquid)' },
+  { id: 'SPICE', name: 'Spice Token' },
+  { id: 'XLM', name: 'Lumen' },
+  { id: 'FOX', name: 'ShapeShift' },
+  { id: 'HNS', name: 'Handshake' },
+  { id: 'XAUT', name: 'XAUt' },
+  { id: 'XTZ', name: 'Tezos' },
+  { id: 'FTT', name: 'FTX Token' },
+  { id: 'SRM', name: 'Serum' },
+  { id: 'COMP', name: 'Compound' },
+  { id: 'SUSHI', name: 'Sushi' },
+  { id: 'YFI', name: 'yearn.finance' },
+  { id: 'UNI', name: 'Uniswap' },
+  { id: 'XAI', name: 'SideShift Token' },
+  { id: 'flexUSD', name: 'flexUSD' },
+  { id: 'WBTC', name: 'WBTC' } ]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containter">
+      <Dropdown title='Select Coin' items={items}/>
+      <Dropdown title='Select Coin' items={items}/>
     </div>
   );
 }
